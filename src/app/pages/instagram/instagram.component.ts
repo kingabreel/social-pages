f1f42cacfe0677310d4f5e5e92202a1b387449cd
kingabreel <gabrielyury441@gmail.com>
2023-11-17 14:41:17 -0300

    instagram redirected

--- src/app/pages/instagram/instagram.component.ts
+++ src/app/pages/instagram/instagram.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./instagram.component.css']
 })
 export class InstagramComponent {
+  loginName: string = '';
+  pass: string = '';
 
+  navigate(){
+    if (this.loginName.length > 5 && this.pass.length > 8) {
+      console.log(this.loginName)
+      console.log(this.pass)
+      window.location.href = 'https://www.instagram.com/';
+    }
+  }
 }
