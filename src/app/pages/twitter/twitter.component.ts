b73282bf4a94ff64de253bb32faab68c3e752499
kingabreel <gabrielyury441@gmail.com>
2023-11-20 21:25:58 -0300

    Twitter finalizado

--- src/app/pages/twitter/twitter.component.ts
+++ src/app/pages/twitter/twitter.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./twitter.component.css']
 })
 export class TwitterComponent {
+  loginWindow: boolean = false;
+  passWindow: boolean = false;
+  username: string = '';
+  
+  changeBackground(){
+    return{
+      'filter': this.loginWindow ? 'brightness(.3)' : 'brightness(1)',
+      'z-index': '-1'
+    }
+  }
 
+  switchLoginWindow(){
+    if(this.loginWindow){
+      this.loginWindow = false;
+    } else {
+      this.loginWindow = true;
+    }
+  }
+
+  switchPasswordWindow(){
+    if(!this.passWindow) {
+      this.username = 'gabriel';
+      this.passWindow = true;
+    } else {
+      this.loginWindow = false;
+      this.passWindow = false;
+    }
+  }
 }
