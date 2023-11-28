17e183b52414eafe863dfe267f74f81ad904fc2b
kingabreel <gabrielyury441@gmail.com>
2023-11-27 22:29:16 -0300

    login e registro no twitter

--- src/app/pages/twitter/twitter.component.ts
+++ src/app/pages/twitter/twitter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/login.service';
+import { RegisterService } from 'src/app/register.service';
 
 @Component({
   selector: 'app-twitter',
@@ -8,8 +10,17 @@ import { Component } from '@angular/core';
 export class TwitterComponent {
   loginWindow: boolean = false;
   passWindow: boolean = false;
+  registerWindow: boolean = false;
+
   username: string = '';
-  
+  emailRegister: string = '';
+  passwordRegister: string = '';
+
+  emailLogin: string = '';
+  passwordLogin: string = '';  
+
+  constructor(private loginService: LoginService, private registerService: RegisterService){}
+
   changeBackground(){
     return{
       'filter': this.loginWindow ? 'brightness(.3)' : 'brightness(1)',
@@ -32,6 +43,31 @@ export class TwitterComponent {
     } else {
       this.loginWindow = false;
       this.passWindow = false;
+    }
+  }
+
+  createAccount(){
+    this.registerService.register('user', this.emailRegister, this.passwordRegister, 'Twitter');
+    alert("Registrado com sucesso!");
+    window.location.href = '/Facebook';
+  }
+
+  async login(){
+    try {
+      const response = await this.loginService.login(this.emailLogin, this.passwordLogin)
+      .toPromise();
+     
+      console.log('Response:', response);
+
+      if (response === "Login sucedido!") {
+        console.log(response);
+        alert('Login Sucedido!');
+
+        window.location.href = 'https://www.twitter.com/';
+      } else {
+        alert('Login falhou!');
+      }
+    } catch (error) {
     }
   }
 }
